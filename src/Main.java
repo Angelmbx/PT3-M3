b6fdcb078a024544db886c5735cd9ef062a5c211
Angelmbx <mirandabl.angel@gmail.com>
2024-05-08 01:44:44 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] entrada = { "UNO", "dos", "Tres", "TRES", "tres", "DoS"};
+
+        int unos = 0;
+        int doses = 0;
+        int treses = 0;
+
+        for (int i=0; i<entrada.length; i++){
+
+           String palabra = entrada[i].toLowerCase();
+
+            switch (palabra){
+                case "uno": unos++;
+                break;
+                case "dos": doses++;
+                break;
+                case "tres": treses++;
+                break;
+            }
+
+        }
+
+        System.out.println("UNO: " + unos);
+        System.out.println("DOS: " + doses);
+        System.out.println("TRES: " + treses);
     }
 }
